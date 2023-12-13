d16488734934645d3a2aa8572fe7164a3c52dd1f
Amit Pandey <64810388+pandeya426@users.noreply.github.com>
2023-12-13 15:30:56 +0530

    Added Dynamic Data and JSON
    Simple dynamic data and JSON file has been added

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS= [
+    {
+        'id':1,
+        'title': 'Data Analyst',
+        'location': ' Mumbai, India',
+        'salary': ' Rs. 10,00,000'
+    },
+        {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': ' Delhi, India',
+        'salary': ' Rs. 15,00,000'
+    },
+        {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': ' Remote',
+        'salary': ' Rs. 120,000'
+        
+    },
+        {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': ' California, USA',
+        'salary': ' $120,000'
+    },
+    
+]
+
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", jobs = JOBS )
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
